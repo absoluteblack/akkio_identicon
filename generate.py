916cfc4380bb5d6407d468381f3f9d1470c0f91d
Connor Cone <connor@sabrient.ai>
2023-03-16 14:48:25 -0500

    Value tweaks to maintain contrast, adding size as a param

--- generate.py
+++ generate.py
@@ -7,20 +7,20 @@ from PIL import Image, ImageColor
 def get_random_hsv():
     hue = 255 * random.random()  # Full hue coverage. We are hue-agnostic.
     sat = 40 + 60 * random.random()  # Sat between 40 and 100; this avoids grey colors.
-    val = 20 * sum([random.random() for x in range(5)])  # A steep bell curve around val 50
+    val = 25 + 15 * sum([random.random() for x in range(5)])  # A steep bell curve around val 62
     return f"hsv({hue}, {sat}%, {val}%)"
 
-def gen_row(foreground):
+def gen_row(foreground, size):
     # Should probably pre-set the array size instead of use a list
     background = ImageColor.getrgb("#0a0f14")  # Following 'gotham' from terminal.sexy for now
-    half = np.array([random.choice([background, foreground]) for x in range(4)], np.uint8)
+    half = np.array([random.choice([background, foreground]) for x in range(size)], np.uint8)
     return np.concatenate((half, half[::-1]))  # There surely is a clever matrix way to do this
 
-def gen_grid():
+def gen_grid(size):
     foreground = ImageColor.getrgb(get_random_hsv())
-    return np.array([gen_row(foreground) for x in range(8)])
+    return np.array([gen_row(foreground, size) for x in range(size*2)])
 
-def gen_image(seed=''.join([random.choice(string.ascii_letters) for i in range(16)])):
+def gen_image(size=8, seed=''.join([random.choice(string.ascii_letters) for i in range(16)])):
     random.seed(seed)  # This is lazy, but it works fine for single thread stuff
-    im = Image.fromarray(gen_grid())
+    im = Image.fromarray(gen_grid(size))
     return im
