e8f407ca534c44f23330a5cf89a0524ac0622a08
Connor Cone <connor@sabrient.ai>
2023-03-16 14:30:55 -0500

    cleanup
    Signed-off-by: Connor Cone <connor@sabrient.ai>

--- generate.py
+++ generate.py
@@ -4,24 +4,11 @@ import numpy as np
 
 from PIL import Image, ImageColor
 
-def get_color():
-    colors = [
-        "#e31a1c",
-        "#31a354",
-        "#dca060",
-        "#3182bd",
-        "#756bb1",
-        "#80b1d3",
-        "#fcfdfe"
-    ]
-    return random.choice(colors)
-
 def get_random_hsv():
     hue = 255 * random.random()  # Full hue coverage. We are hue-agnostic.
     sat = 40 + 60 * random.random()  # Sat between 40 and 100; this avoids grey colors.
     val = 20 * sum([random.random() for x in range(5)])  # A steep bell curve around val 50
     return f"hsv({hue}, {sat}%, {val}%)"
-
 
 def gen_row(foreground):
     # Should probably pre-set the array size instead of use a list
@@ -30,7 +17,6 @@ def gen_row(foreground):
     return np.concatenate((half, half[::-1]))  # There surely is a clever matrix way to do this
 
 def gen_grid():
-    # foreground = ImageColor.getrgb(get_color())
     foreground = ImageColor.getrgb(get_random_hsv())
     return np.array([gen_row(foreground) for x in range(8)])
 
